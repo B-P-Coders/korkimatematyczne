cc480549b0cef4ba4b141d5da7591263184a73f7
Piotr Nowakowski <piotrek02nowakowski@gmail.com>
2023-11-11 23:17:09 +0100

    add generated file

---
+++ lib/src/journal.g.dart
@@ -0,0 +1,18 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'journal.dart';
+
+// **************************************************************************
+// LiteralEmbeddingGenerator
+// **************************************************************************
+
+const _$bookIdx = [
+  {
+    "name": "LO4 Matematyka kl3 rozszerzona",
+    "index": "/p/matematyka-zbior-zadan-do-liceow-i_26.html"
+  },
+  {
+    "name": "LO4 Matematyka kl4 rozszerzona",
+    "index": "/p/lo-4-letnie-zbior-zadan-do-liceow-i.html"
+  }
+];
